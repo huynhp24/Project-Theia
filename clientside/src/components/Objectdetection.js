7018262db2b91b520448e119e61076ab255976f8
Nhi Truong <tamnhi96@gmail.com>
2020-10-26 19:28:51 -0700

    create two new buttons

---
+++ clientside/src/components/Objectdetection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Objectdetection extends Component {
+
+    render()   {
+        return (
+            <div>
+            <h1>Object Detection App</h1>
+            </div>
+        );
+    }
+}
+
+export default Objectdetection;
---
+++ clientside/src/components/UploadButtons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+  
+  export default function UploadButtons() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          multiple
+          type="file"
+        />
+        <label htmlFor="contained-button-file">
+            <Button variant="contained" color="primary" component="span" startIcon={<CloudUploadIcon />}>
+                Choose File
+            </Button>
+        </label>
+      </div>
+    );
+  }
+
