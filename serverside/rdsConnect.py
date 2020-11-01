b018291cc045e0ee63f2dad41d8f8d9a5f4ae895
huynhp24 <huynhp24@gmail.com>
2020-11-01 12:35:20 -0800

    Added rdsConnect test file

---
+++ serverside/rdsConnect.py
@@ -0,0 +1,41 @@
+import pymysql
+
+host=""
+port=3306
+dbname="theia_db"
+user="admin"
+password=""
+
+conn = pymysql.connect(host, user=user,port=port,passwd=password, db=dbname)
+
+
+def insertTest():
+    insert_statement = "INSERT INTO Account(id,name,username,password) VALUES (2,'John Bro', 'yuser', 'passy');"
+    try:
+        cursorObject        = conn.cursor()                                     
+        sqlQuery            = insert_statement
+        cursorObject.execute(sqlQuery)
+        cursorObject.execute("SELECT * FROM Account")
+        rows = cursorObject.fetchall()
+        print(rows)
+
+    except Exception as e:
+        print("Exeception occured:{}".format(e))
+    finally:
+        conn.commit()
+        print("Data Successfully Inserted")   
+        conn.close()
+
+def queryTest():
+    try:
+        cursorObject        = conn.cursor()                                     
+        cursorObject.execute("SELECT * FROM Account")
+        rows = cursorObject.fetchall()
+        print(rows)
+
+    except Exception as e:
+        print("Exeception occured:{}".format(e))
+    finally:
+        conn.close()
+
+queryTest()
