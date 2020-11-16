bda40116cffc08101f0a5fc1c0c65521cf2b4784
YenniLamb <47261678+YenniLamb@users.noreply.github.com>
2020-11-15 17:52:43 -0800

    add runConfig.py for (rekog & textract), add config.ini

---
+++ serverside/oldPythonFile/detect.py
@@ -0,0 +1,24 @@
+import boto3
+
+# Document
+s3BucketName = "bucket-image"
+documentName = "coffeeText.JPG"
+
+# Amazon Textract client
+textract = boto3.client('textract')
+
+# Call Amazon Textract
+response = textract.detect_document_text(
+    Document={
+        'S3Object': {
+            'Bucket': s3BucketName,
+            'Name': documentName
+        }
+    })
+
+#print(response)
+
+# Print detected text
+for item in response["Blocks"]:
+    if item["BlockType"] == "LINE":
+        print ('\033[94m' +  item["Text"] + '\033[0m')
