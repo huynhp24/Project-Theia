6b48af678139165f6ac7204d37488e055d6a2465
testGitd <suntanfl@yahoo.com>
2021-01-03 01:37:08 -0800

    testrabbitmq

---
+++ serverside/RabbitMQ/url_queue_reciever.py
@@ -0,0 +1,17 @@
+import pika
+
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='urlQueue')
+
+def callbackfunc(ch, method, properties, text):
+    print(" Recieved message from sender: %r" % text)
+
+channel.basic_consume(queue='urlQueue',
+                      on_message_callback=callbackfunc,
+                      auto_ack=True)
+print('Waiting to get messages from sender')
+channel.start_consuming()
