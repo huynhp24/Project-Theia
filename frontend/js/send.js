e9129f532534f3382d0768e1e5169f22ed439eef
Nhi Truong <tamnhi96@gmail.com>
2021-02-11 12:47:42 -0800

    Migration commit

---
+++ frontend/js/send.js
@@ -0,0 +1,5 @@
+    function myFunction() {
+    //   document.getElementById("frm1").submit();
+        var nameValue = document.getElementById("userinput").value;
+        console.log(nameValue);
+    }
