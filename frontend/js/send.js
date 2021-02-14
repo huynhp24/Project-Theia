0df4d961feef0976dc004489739ebcd667752c72
Nhi Truong <tamnhi96@gmail.com>
2021-02-13 16:02:26 -0800

    update front-end code for 2 buttons

--- frontend/js/send.js
+++
@@ -1,5 +0,0 @@
-    function myFunction() {
-    //   document.getElementById("frm1").submit();
-        var nameValue = document.getElementById("userinput").value;
-        console.log(nameValue);
-    }
