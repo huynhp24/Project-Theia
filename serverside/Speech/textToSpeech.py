dfb68c855bb525acc0b528c569daafd99f6dc2b5
tienpham0203 <46726728+tienpham0203@users.noreply.github.com>
2021-03-31 22:11:27 -0700

    Update textToSpeech.py

--- serverside/Speech/textToSpeech.py
+++ serverside/Speech/textToSpeech.py
@@ -2,11 +2,11 @@
 import azure.cognitiveservices.speech as speechsdk
 import uuid
 
-speech_key="4abfe9e3418d46deab7c6ec2b27f9fb7"
-service_region = "eastus"
+speech_key=""
+service_region = ""
 
 
-#text to speech function, input is a string, output is audio from your speaker
+
 def text_to_speech_function(text):
     speech_config = speechsdk.SpeechConfig(subscription=speech_key,region=service_region)
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
