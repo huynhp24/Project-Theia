3318c77296aa3d853ddad1b62cbc0e3295e58247
Huynh Phan <huynhp24@gmail.com>
2021-05-01 15:41:17 -0700

    making it pretty and counted

--- serverside/Nat_Lang_Gen.py
+++ serverside/Nat_Lang_Gen.py
@@ -242,12 +242,15 @@ def GenerateSummary(labels,textExtracted):
         locs = ""
         if(len(pretty_parents[label]['Instances'])>1):
             loc = location(pretty_parents,label)
-            loc = set(loc)
             if(len(loc)>0):
-                if(len(loc)>4):
+                if(len(set(loc))>4):
                     locs="They are located throughout the image. "
                 else:
-                    locs="They are located " + ' and '.join(loc)+'. '
+                    locs="Their locations: "
+                    pretty_loc = []
+                    for it in set(loc):
+                        pretty_loc.append(str(loc.count(it)) + ' ' + loc)
+                    locs+= ' and '.join(pretty_loc)+'. '
             prefix = 'are ' + str(len(pretty_parents[label]['Instances']))
             suffix = 's'
 
