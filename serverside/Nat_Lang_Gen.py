6d1aef1cd945ffb998e385ad96335fe2c094e2c0
Huynh Phan <huynhp24@gmail.com>
2021-05-01 15:48:00 -0700

    trying to be prettier

--- serverside/Nat_Lang_Gen.py
+++ serverside/Nat_Lang_Gen.py
@@ -281,17 +281,17 @@ def GenerateSummary(labels,textExtracted):
             loc = location(pretty_loners,label)
             if(len(loc)>0):
                 if(len(set(loc))>4):
-                    locs="They are located throughout the image. "
+                    locs="(located throughout the image"
                 else:
-                    locs="Their locations: "
+                    locs="(their locations: "
                     pretty_loc = []
                     for it in set(loc):
                         pretty_loc.append(str(loc.count(it)) + ' ' + it)
-                    locs+= ' and '.join(pretty_loc)+'. '
+                    locs+= ' and '.join(pretty_loc)
             prefix = 'are ' + str(len(pretty_loners[label]['Instances']))
             suffix = 's'
         
-        loner_list.append(prefix +" " + label+ suffix+'. ' + locs)
+        loner_list.append(prefix +" " + label+ suffix+', ' + locs+ ') ')
 
     if(len(pretty_loners)>0):
         if(len(loner_list)>1):
